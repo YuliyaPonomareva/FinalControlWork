406ba4722903012278c5fa0a11e95d0622c58fa0
yuliya <liana-yuliana@list.ru>
2023-07-01 18:17:26 +0300

    Added file FinalTask

---
+++ FinalTask/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+Примеры:
+[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+[“Russia”, “Denmark”, “Kazan”] → []  
+*/
+
+Console.Clear();
+
+Console.Write("Введите количество элементов массива: ");
+int count = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите элементы массива:");
+string[] array = GetArray(count);
+Console.WriteLine();
+Console.WriteLine("Первый массив:");
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Измененный массив:");
+string[] array1 = FromArray(array);
+PrintArray(array1);
+
+string[] GetArray(int count)
+{
+    string[] arr = new string[count];
+    for (int i = 0; i < count; i++)
+    {
+        arr[i] = Console.ReadLine()!;
+    }
+    return arr;
+}
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+string[] FromArray(string[] mass)
+{
+    string[] mass1 = new string[mass.Length];
+    int size = 0;
+    foreach (string el in mass)
+    {
+        if(el.Length<=3)
+        {
+            mass1[size] = el;
+            size++;
+        }
+    }
+    return mass1;
+}
